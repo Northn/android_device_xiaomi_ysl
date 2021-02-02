93fc03992cac660dfd7a9bec2149e7a63a37f61b
SunnyRaj84348 <sunnyraj7461@gmail.com>
2021-02-02 09:20:48 +0000

    ysl: Tweaks for better RAM management
    Change-Id: I42d26f6670b7f6b7a59e841b54cf21531b9fdaff

--- prop.mk
+++ prop.mk
@@ -254,6 +254,18 @@ persist.sys.fflag.override.settings_network_and_internet_v2=true
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.rcs.supported=1
 
+# Ram Management
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.config.fha_enable=true \
+ro.sys.fw.bg_apps_limit=32 \
+ro.config.dha_cached_max=16 \
+ro.config.dha_empty_max=42 \
+ro.config.dha_empty_init=32 \
+ro.config.dha_lmk_scale=0.545 \
+ro.config.dha_th_rate=2.3 \
+ro.config.sdha_apps_bg_max=64 \
+ro.config.sdha_apps_bg_min=8
+
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 ro.surface_flinger.protected_contents=true
